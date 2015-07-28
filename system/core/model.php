36ceaee6aa7dbd1ed6ba6f4f1af6129a0bb946d5
Pawan Reddy <pawan12reddy@gmail.com>
2015-07-28 12:26:49 +0530

    Create model.php

---
+++ system/core/model.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Model {
+
+	protected $registry;
+
+	public function __construct($registry) {
+		$this->registry = $registry;
+	}
+
+	public function __get($key) {
+		return $this->registry->get($key);
+	}
+
+	public function __set($data,$key) {
+		$this->registry->set($data,$key);
+	}
+
+}
