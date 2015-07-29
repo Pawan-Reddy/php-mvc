8f2f9488d65c918cf3777f0dce0473eebc0a9862
Pawan Reddy <pawan12reddy@gmail.com>
2015-07-29 15:53:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,16 @@
 <?php
 	
-	defined('VERSION') OR exit('No direct script access allowed');
+defined('VERSION') OR exit('No direct script access allowed');
 
 	
-	//DB
-	define('DB_HOSTNAME','localhost');
-	define('DB_USERNAME','root');
-	define('DB_PASSWORD','');
-	define('DB_DATABASE','dbname');
-	define('DB_PREFIX', 'pre');
+//DB
+define('DB_HOSTNAME','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_DATABASE','dbog');
+define('DB_PREFIX', 'bog');
 
-	//Directory location
-
-	/*
-	* Comment Out below line if sub directory exists and specify the path
-	*/
-	//define('APP_SUBDIR','experiments/sub-directory/');
-	
-	define('APP_DIR', str_replace('\\','/',dirname(__FILE__)) . '/');
-
-	define('APP_SYSTEM', APP_DIR . 'system/' );
+//Directory location
+define('APP_DIR', str_replace('\\','/',dirname(__FILE__)) . '/');
+define('APP_SYSTEM', APP_DIR . 'system/' );
 	
