9c94a0ef8c48ac5ae863c9ed281f098703792315
Pawan Reddy <pawan12reddy@gmail.com>
2015-07-31 11:02:40 +0530

    Update loader.php

--- system/core/loader.php
+++ system/core/loader.php
@@ -1,9 +1,9 @@
 <?php
 
 final class Loader {
-	private $registry;
+ private $registry;
 
-	public function __construct($registry) {
+ 	public function __construct($registry) {
 		$this->registry = $registry;
 	}
 
@@ -19,7 +19,28 @@ final class Loader {
 	}
 
 	public function view($tpl,$data) {
+		
+		$file = APP_FRONT . 'view/' . $tpl . '.tpl';
 
+		if(file_exists($file)){
+
+			extract($data);
+
+			ob_start();
+
+			require($file);
+
+			$output = ob_get_contents();
+				
+			ob_end_clean();
+
+			return $output;
+
+		} else {
+
+			trigger_error('Fail to load template file ' . $file);
+			exit();
+		}
 	}
 
 
