a35d7824368a755a52427a24fcfc34b6fdf3ed9c
Pawan Reddy <pawan12reddy@gmail.com>
2015-08-11 13:37:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('VERSION','0.1.a');
+	define('VERSION','0.1.b');
 
 	if(file_exists('config.php')) {
 		require_once('config.php');
@@ -10,16 +10,27 @@
 		exit('config file not available ');
 	}
 	
+	
 	//startup.php pre-loads all initial files
 	require_once(APP_SYSTEM . 'startup.php');
-
+	
 	$registry = new Registry();
 
 	$loader = new Loader($registry);
 
 	$registry->set('load',$loader);
 
-	// Database
-	$db = new DB(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-	$registry->set('db', $db);
+	$request = new Request();
+
+	$registry->set('request',$request);
+	
+	if(isset($request->get['url']) && isset($request->get['args'])) {
+		$route = new Route($request->get['url'],$request->get['args']);
+	} else if(isset($request->get['url'])){
+		$route = new Route($request->get['url']);
+	} else {
+		$route = new Route('common/home');
+	}
+
+	echo $route->run($registry);
 
