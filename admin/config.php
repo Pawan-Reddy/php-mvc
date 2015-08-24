bb3a0a54b5488a80af9d12289cf12d3ca0426133
Pawan Reddy <pawan12reddy@gmail.com>
2015-08-24 13:34:29 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,16 +1,16 @@
 <?php
 	
-	defined('VERSION') OR exit('No direct script access allowed');
+defined('VERSION') OR exit('No direct script access allowed');
 
-	
-	//DB
-	define('DB_HOSTNAME','localhost');
-	define('DB_USERNAME','root');
-	define('DB_PASSWORD','');
-	define('DB_DATABASE','dbog');
-	define('DB_PREFIX', 'bog');
+//DB
+define('DB_HOSTNAME','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_DATABASE','database_name');
+define('DB_PREFIX', 'prefix'); // change 'prefix' to your custom prefix
+define('DB_DRIVER', 'mysqli'); //set to either MySQLi or mPDO
 
-	//Directory location
-	define('APP_MAIN', str_replace('\\','/',dirname(__FILE__)) . '/');
-	define('APP_SYSTEM', APP_MAIN . 'system/' ); //system files
-	define('APP_DIR', APP_MAIN . 'admin/' ); //backend controller,view model files
+//Directory location
+define('APP_MAIN', str_replace('\\','/',dirname(__FILE__)) . '/');
+define('APP_SYSTEM', APP_MAIN . 'system/' ); //system files
+define('APP_DIR', APP_MAIN . 'front/' ); //frontend controller,view model files
