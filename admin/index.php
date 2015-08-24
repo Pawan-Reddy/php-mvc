02c3fe8d83dbaae4755faa449a7c7816a2a08b46
Pawan Reddy <pawan12reddy@gmail.com>
2015-08-24 12:57:20 +0530

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,39 @@
+<?php
+
+	define('VERSION','0.1.b');
+
+	if(file_exists('config.php')) {
+		require_once('config.php');
+	}
+
+	if(!defined('APP_DIR')) {
+		exit('config file not available ');
+	}
+	
+	//startup.php pre-loads all initial files
+	require_once(APP_SYSTEM . 'startup.php');
+	
+	$registry = new Registry();
+
+	$loader = new Loader($registry);
+
+	$registry->set('load',$loader);
+
+	$request = new Request();
+
+	$registry->set('request',$request);
+	
+	// Database
+	$db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	$registry->set('db', $db);
+	
+	if(isset($request->get['url']) && isset($request->get['args'])) {
+		$route = new Route($request->get['url'],$request->get['args']);
+	} else if(isset($request->get['url'])){
+		$route = new Route($request->get['url']);
+	} else {
+		$route = new Route('common/home');
+	}
+
+	echo $route->run($registry);
+
